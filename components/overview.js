d4ecf1f983fea51627bc6f93efed512aabd5d93c
pekoz.eth <foo@bar.org>
2022-01-22 15:45:58 +0100

    added a simple chart

--- components/overview.js
+++ components/overview.js
@@ -1,20 +1,33 @@
 import getMintsBurnsTransfers from "../utils/getMintsBurnsTransfers"
+import { Bar } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import styles from '../styles/Home.module.css'
 
 export default function Overview({ transfers, tokens }) {
     const data = getMintsBurnsTransfers(transfers)
+    const dataset = {
+        label: 'N° tokens', data: [data.mints, data.burns, data.transfers], borderWidth: 1
+    }
+    const datachart = {
+        props: { labels: ["MINTS", "BURNS", "TRANSFERS"], datasets: [dataset] }
+    }
     return (
-        <div>
-            <p>Active card: {tokens.length}</p>
-
-            <p>
-                Total Mints: {data.mints}
-            </p>
-            <p>
-                Total Burns: {data.burns}
-            </p>
-            <p>
-                Total Transfers: {data.transfers}
-            </p>
+        <div className={styles.grid}>
+            <div>
+                <p>Active card: {tokens.length}</p>
+                <p>Total Mints: {data.mints}</p>
+                <p>Total Burns: {data.burns}</p>
+                <p>Total Transfers: {data.transfers}</p>
+            </div>
+            <div>
+                <Bar
+                    data={datachart.props}
+                    width={400}
+                    height={200}
+                />
+            </div>
         </div>
+
     )
 }
+
